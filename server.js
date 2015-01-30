ec08514b652d035561e578d84c0a91a1e911caf7
Lukas Martinelli <me@lukasmartinelli.ch>
2015-01-30 20:42:19 +0100

    Worked on autocomplete search

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
+/*eslint new-cap:0 */
 'use strict';
-var path = require('path');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var autocomplete = require('./autocomplete');
-
 var options = {
     accessToken: process.env.GITHUB_TOKEN,
     port: process.env.VCAP_APP_PORT || 3000
@@ -14,7 +9,21 @@ if(!options.accessToken) {
     throw 'No Github Access token specified.';
 }
 
+var path = require('path');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var GitHubApi = require('github');
+
+var github = new GitHubApi({
+    version: '3.0.0',
+    timeout: 5000,
+    headers: { 'user-agent': 'flinter' }
+});
+
+var autocomplete = require('./autocomplete')(github);
+
 app.use(express.static(path.join(__dirname, '/public')));
-app.get('/autocomplete/:search', autocomplete.find);
+app.get('/search', autocomplete.find);
 
 http.listen(options.port);
